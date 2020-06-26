8eaa7755fc29ac16ed0b69fd476846937ae24145
hamwop <hanwo2052@gmail.com>
2020-06-26 18:21:16 +0900

    Kmeans : 시각화 추가 PCA : dev ~ing

---
+++ PCA_Kmeans_cluster.py
@@ -0,0 +1,42 @@
+from sklearn.cluster import KMeans
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+
+players = pd.read_csv("player_data.csv")
+
+feature = players[["speed", "shoot", "pass", "dribble", "defense", "physical"]]
+name = players["name"]
+
+pca = decomposition.PCA(n_components=6).fit(feature)
+reduced_X = pca.transform(feature)
+
+
+model = KMeans(n_clusters=3)
+model.fit(reduced_X)
+result_kmeans = model.predict(feature)
+
+predict = pd.DataFrame(result_kmeans)
+predict.columns = ['predict']
+
+r = pd.concat([feature, predict], axis=1)
+print(r.head(15))
+for idx, i in enumerate(r['predict']):
+    if i == 0:
+        r['predict'][idx] = "Striker"
+    elif i == 1:
+        r['predict'][idx] = "MidField"
+    elif i == 2 :
+        r['predict'][idx] = "Defense"
+
+g = sns.scatterplot(x="speed", y="shoot", hue = 'predict', style = 'predict', data=r)
+sns.pairplot(r, hue="predict", markers=["o", "s", "D"])
+plt.xlabel('Speed')
+plt.ylabel('Shoot')
+g.set_title("speed & shoot")
+g.legend(loc='lower right', bbox_to_anchor=(1.25, 0.5), ncol=1)
+plt.show()
